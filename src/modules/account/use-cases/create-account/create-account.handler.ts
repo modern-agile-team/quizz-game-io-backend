0f52d5e09cda87d501f8dcfb3a79befb52d5a191
rrgks6221 <rrgks@naver.com>
2025-06-29 19:54:39 +0900

    feat: 계정 생성 시 이벤트 저장

--- src/modules/account/use-cases/create-account/create-account.handler.ts
+++ src/modules/account/use-cases/create-account/create-account.handler.ts
@@ -9,6 +9,11 @@ import {
 } from '@module/account/repositories/account/account.repository.port';
 import { CreateAccountCommand } from '@module/account/use-cases/create-account/create-account.command';
 
+import {
+  EVENT_STORE,
+  IEventStore,
+} from '@core/event-sourcing/event-store.interface';
+
 @CommandHandler(CreateAccountCommand)
 export class CreateAccountHandler
   implements ICommandHandler<CreateAccountCommand, Account>
@@ -16,16 +21,10 @@ export class CreateAccountHandler
   constructor(
     @Inject(ACCOUNT_REPOSITORY)
     private readonly accountRepository: AccountRepositoryPort,
+    @Inject(EVENT_STORE) private readonly eventStore: IEventStore,
   ) {}
 
   async execute(command: CreateAccountCommand): Promise<Account> {
-    const account = Account.create({
-      role: command.role,
-      signInType: command.signInType,
-      username: command.username,
-      password: command.password,
-    });
-
     const existingAccount = await this.accountRepository.findOneByUsername(
       command.username,
     );
@@ -34,7 +33,16 @@ export class CreateAccountHandler
       throw new AccountUsernameAlreadyOccupiedError();
     }
 
+    const account = Account.create({
+      role: command.role,
+      signInType: command.signInType,
+      username: command.username,
+      password: command.password,
+    });
+
     await this.accountRepository.insert(account);
+
+    await this.eventStore.storeAggregateEvents(account);
 
     return account;
   }
