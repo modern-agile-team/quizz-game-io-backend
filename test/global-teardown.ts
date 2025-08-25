7791a5daf782fbc17ba3cb5c0c462fa7f9f90651
LeeSeokHo <rrgks@naver.com>
2025-08-25 15:18:28 +0900

    test: 테스트 환경에서 redis를 테스트컨테이너 인스턴스를 사용하도록 설정

--- test/global-teardown.ts
+++ test/global-teardown.ts
@@ -1,3 +1,8 @@
 module.exports = async function () {
-  await globalThis.__POSTGRES_CONTAINER__.stop();
+  if (global.__REDIS_CONTAINER__) {
+    await global.__REDIS_CONTAINER__.stop();
+  }
+  if (global.__POSTGRES_CONTAINER__) {
+    await global.__POSTGRES_CONTAINER__.stop();
+  }
 };
