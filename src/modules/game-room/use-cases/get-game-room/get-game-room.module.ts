29f73a5e07e31a3c1948555520d27d97e1b0c7d5
seokho <rrgks@naver.com>
2025-09-23 15:34:33 +0900

    게임방 상세조회 REST API 추가 (#83)
    feat: 게임방 상세조회 REST API

--- src/modules/game-room/use-cases/get-game-room/get-game-room.module.ts
+++ src/modules/game-room/use-cases/get-game-room/get-game-room.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { GameRoomRepositoryModule } from '@module/game-room/repositories/game-room/game-room.repository.module';
+import { GameRoomAccessControlModule } from '@module/game-room/services/game-room-access-control/game-room-access-control.module';
+import { GetGameRoomController } from '@module/game-room/use-cases/get-game-room/get-game-room.controller';
 import { GetGameRoomHandler } from '@module/game-room/use-cases/get-game-room/get-game-room.handler';
 
 @Module({
-  imports: [GameRoomRepositoryModule],
+  controllers: [GetGameRoomController],
+  imports: [GameRoomAccessControlModule, GameRoomRepositoryModule],
   providers: [GetGameRoomHandler],
 })
 export class GetGameRoomModule {}
