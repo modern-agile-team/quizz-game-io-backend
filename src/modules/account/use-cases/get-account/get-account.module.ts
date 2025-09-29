e398aeac318e2fb49b743e63c17c06e50dec67a4
seokho <rrgks@naver.com>
2025-09-29 16:41:23 +0900

    내 계정 조회 REST API (#91)
    feat: 내 계정 조회 REST API

--- src/modules/account/use-cases/get-account/get-account.module.ts
+++ src/modules/account/use-cases/get-account/get-account.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AccountRepositoryModule } from '@module/account/repositories/account/account.repository.module';
+import { GetAccountController } from '@module/account/use-cases/get-account/get-account.controller';
 import { GetAccountHandler } from '@module/account/use-cases/get-account/get-account.handler';
 
 @Module({
+  controllers: [GetAccountController],
   imports: [AccountRepositoryModule],
   providers: [GetAccountHandler],
 })
